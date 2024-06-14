d17cdf5b1c10ffaf105542f4988e77ce76d94f48
Ringo Van den Hende <ringo.vandenhende@faros.be>
2024-06-14 09:34:30 +0200

    add security

--- src/main/java/be/faros/sandwich/api/SandwichApiApplication.java
+++ src/main/java/be/faros/sandwich/api/SandwichApiApplication.java
@@ -2,8 +2,9 @@ package be.faros.sandwich.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class SandwichApiApplication {
 
     public static void main(String[] args) {
