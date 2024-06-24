f228960d200e834c4ee750f0634f790148fb6029
Ringo Van den Hende <ringo.vandenhende@faros.be>
2024-06-24 10:08:07 +0200

    add scope protection

--- src/main/java/be/faros/sandwich/api/controller/SandwichControllerImpl.java
+++ src/main/java/be/faros/sandwich/api/controller/SandwichControllerImpl.java
@@ -4,6 +4,11 @@ import be.faros.sandwich.api.dto.SandwichDTO;
 import be.faros.sandwich.api.service.SandwichService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +29,10 @@ public class SandwichControllerImpl implements SandwichController {
 
     @Override
     @GetMapping()
+    @PreAuthorize("hasAuthority('read')")
     public ResponseEntity<List<SandwichDTO>> getSandwiches() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return ResponseEntity.ok(sandwichService.getSandwiches());
     }
 }
+
